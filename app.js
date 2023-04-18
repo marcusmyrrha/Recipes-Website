b8ecfc26c300a32c3c0097fc1eb6d8c030ecf41f
Marcus Myrrha <m.myrrha.n@gmail.com>
2023-04-18 14:44:19 -0300

    Full project - v1.0

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const getElement = (selector) =>{
+
+    const element = document.querySelector(selector);
+    if(element) return element
+    throw Error('Please double check your class names, there is no ${selector} class')
+}
+
+const links = getElement('.nav__links')
+const navBtnDOM = getElement('.nav__btn')
+
+navBtnDOM.addEventListener('click',()=>{
+    links.classList.toggle('show__links')
+})
+
+const date = getElement ('#date');
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
